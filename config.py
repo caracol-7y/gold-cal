3b2f66314b7799d565593a8fc5c2af006765ab96
caracol-7y <caracol.7y@gmail.com>
2026-04-03 22:08:16 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 # config.py
-
-# 金属のカテゴリと、それに属するキーの定義
 METAL_CATEGORIES = {
     "Gold": ["Gold_Ingot", "K24", "K22", "K21.6", "K20", "K18", "K14", "K10", "K9"],
     "Platinum": ["Pt_Ingot", "Pt1000", "Pt950", "Pt900", "Pt850"],
@@ -8,32 +6,10 @@ METAL_CATEGORIES = {
     "Palladium": ["Pd_Ingot"]
 }
 
-# プログラム上のキーと、画面表示用の名称の対応表
 OPTIONS_MAP = {
-    "Gold_Ingot": "Gold Bar",
-    "K24": "K24",
-    "K22": "K22",
-    "K21.6": "K21.6",
-    "K20": "K20",
-    "K18": "K18",
-    "K14": "K14",
-    "K10": "K10",
-    "K9": "K9",
-    "Pt_Ingot": "Platinum Bar",
-    "Pt1000": "Pt1000",
-    "Pt950": "Pt950",
-    "Pt900": "Pt900",
-    "Pt850": "Pt850",
-    "Silver_Ingot": "Silver Bar",
-    "Sv1000": "Sv1000",
-    "Sv925": "Sv925",
-    "Pd_Ingot": "Pd Bar"
+    "Gold_Ingot": "Gold Bar", "K24": "K24", "K22": "K22", "K21.6": "K21.6", "K20": "K20", "K18": "K18", "K14": "K14", "K10": "K10", "K9": "K9",
+    "Pt_Ingot": "Platinum Bar", "Pt1000": "Pt1000", "Pt950": "Pt950", "Pt900": "Pt900", "Pt850": "Pt850",
+    "Silver_Ingot": "Silver Bar", "Sv1000": "Sv1000", "Sv925": "Sv925", "Pd_Ingot": "Pd Bar"
 }
 
-# アプリの初期値設定
-DEFAULT_SETTINGS = {
-    "weight": 1.0,
-    "rate_sell": 90,
-    "rate_buy": 5,
-    "use_bukin": False
-}
+DEFAULT_SETTINGS = {"weight": 1.0, "rate_sell": 90, "rate_buy": 5, "use_bukin": False}
